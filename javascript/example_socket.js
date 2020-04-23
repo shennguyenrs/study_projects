f8bc83c82a0e7838a9f8b3351bd534b7a1d9b2b6
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-04-24 00:49:07 +0300

    updated example for socket

--- javascript/example_socket.js
+++ javascript/example_socket.js
@@ -5,7 +5,6 @@ const port = 8080;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/nodes_modules'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/example_socket_index.html');
 });
@@ -14,10 +13,38 @@ server.listen(port, () => {
   console.log('App is listening on port ' + port);
 });
 
-io.on('connection', client => {
-  console.log('A client connected');
+// Broadcast
+var client = 0;
 
-  client.on('diconnect', () => {
-    console.log('A client diconnect');
+io.on('connection', socket => {
+  client++;
+  socket.emit('broadcast', {description: client + ' clients connected'});
+  socket.on('disconnect', () => {
+    client--;
+    socket.emit('broadcast', {description: client + ' clients connected'});
   });
 });
+
+/*
+// Whenever someone connect, this code will be excuted
+io.on('connection', socket => {
+  console.log('A client connected');
+
+  // Send a message when timeout is 5 seconds
+  setTimeout(() => {
+    socket.send('You have been on site for 5 seconds');
+    // Send an emit object
+    socket.emit('testEvent', {description: 'A test event'});
+    console.log('Client has been on site for 5 seconds');
+  }, 5000);
+
+  // When the user is diconnect from the site
+  socket.on('disconnect', () => {
+    console.log('A client diconnect');
+  });
+  // Receive event from Client
+  socket.on('ClientEvent', data => {
+    console.log(data);
+  });
+});
+*/
