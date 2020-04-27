9f4c9d758166be45cc98ba936c79b53204cc43bc
shennguyenrs <shennguyen.redshinobi96@gmail.com>
2020-04-28 02:52:47 +0300

    update docstring

--- python/iban_checker/checker.py
+++ python/iban_checker/checker.py
@@ -2,21 +2,20 @@ def checker(iban_code):
     """
     Check IBAN number.
 
-    Parameter:
-        iban_code (str): IBAN number inputted by user.
-        country_code (str): Country code included first 2 letter from IBAN number.
-        c_name (str): Country name.
-        c_length(int): Default country length of IBAN.
-        iban_length (int): The length of inputted IBAN.
-        index (int): The number of country code and check digits in IBAN number. Default is 4.
-        new_iban (str): New IBAN string re-arraged and prepared for modulo.
-        divisor (int): Divisor for modulo. Default is 97 due to ISO 7064.
-        valid (int): The result from modulo calculation. 1 is valid, else means not valid.
-        result (int or array): If result = 0, IBAN is invalid. Else it will print information of IBAN.
+    Parameters:
+    -----------
+        iban_code : str
+            IBAN number inputted by user.
 
-    Return:
-        IBAN number is valid or invalid.
-        If IBAN number is valid, the information should be printed: Country name, Bank name, BIC.
+    Returns:
+    --------
+        result : int or array
+            If result = 0, IBAN is invalid.
+            if result = 1, IBAN is wrong length.
+            Else it will print information of IBAN: Country, Bank and BIC.
+
+    Raises:
+    -------
 
     """
 
@@ -37,28 +36,24 @@ def checker(iban_code):
     c_name = toCountry(country_code)[0]
     c_length = toCountry(country_code)[1]
 
-    iban_length = len(iban_code)
-
     # Check inputted IBAN length
-    if c_length != iban_length:
+    if c_length != len(iban_code):
         print("Your IBAN number is not correct in length! Please enter again!")
-        result = 0
-    else:
-        # Convert Country code to number
-        c_number = toNumber(country_code)
+        result = 1
 
-        # Merge country_code and re-arrage IBAN to new string
+    else:
+        # Re-arrage IBAN to new string
         new_iban = ""
         index = 4
 
         for i in range(index, c_length - index):
             new_iban += iban_code[i]
 
-        new_iban += c_number
+        # Add converted country code to new IBAN
+        new_iban += toNumber(country_code)
 
         # Check IBAN Valid or not
-        divisor = 97
-        valid = mod(new_iban, c_length, divisor)
+        valid = mod(new_iban, c_length)
 
         if valid == 1:
             result = []
