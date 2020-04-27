c58407fd9f16f1a9daf006e95c41f0ddea911180
shennguyenrs <shennguyen.redshinobi96@gmail.com>
2020-04-28 02:53:19 +0300

    update docstring

--- python/iban_checker/modules/mod.py
+++ python/iban_checker/modules/mod.py
@@ -1,17 +1,27 @@
-def mod(code, length, divisor):
+def mod(code, length):
     """
     Return modulo of big string number code with divisor.
 
     Paramaters:
-        code (str): IBAN string.
-        length (int): Length of the code by country.
-        divisor (int): By default, divisor is 97 by ISO 7064.
+    -----------
+        code : str
+            IBAN string.
+        length : int
+            Length of the code by country.
 
     Returns:
-        rem (int): Remander of modulo calculation
+    --------
+        rem : int
+            Remander of modulo calculation
+
+    Raises:
+    -------
+
     """
 
     rem = 0
+    # By default, divisor is 97 by ISO 7064.
+    divisor = 97
 
     for i in range(0, length):
         rem = (rem*10 + int(code[i]))%divisor
