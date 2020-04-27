f8391f2c2448e48a2e53ec44e68e83020d56d210
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-04-27 15:14:25 +0300

    update checker

--- python/iban_checker/checker.py
+++ python/iban_checker/checker.py
@@ -5,13 +5,22 @@ Parameter:
 
 Return:
 
-
 """
 # Import modules
-
+from iban_checker.modules.code_to_country import toCountry
 
 def checker():
 
-    print("Input IBAN number (without spaces): ")
-    iban = str(input())
+    print("Input IBAN number: ")
+    iban_code = str(input())
 
+    # Remove all spaces
+    if iban_code.isspace():
+        iban_code.replace(" ", "")
+
+    # Check country and default length
+    country_code = ""
+    country_code = iban_code[0] + iban_code[1]
+
+    c_name = toCountry(country_code)[0]
+    c_length = toCountry(country_code)[1]
