fd45176fdf76415d2ee5997fa7b799283d24a5ba
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-04-28 21:39:15 +0300

    add code file path

--- python/iban_checker/modules/code_to_number.py
+++ python/iban_checker/modules/code_to_number.py
@@ -1,42 +1,54 @@
 import csv
+import os
+from iban_checker.modules.file_path import file_path
 
 def toNumber(code):
     """
     Convert country code to number.
 
-    Parameter:
-        code (str): Country code with 2 letter.
-        let_num (array): Array that contains table of letters to numbers.
-        fnum (int): First number converted from code.
-        snum (int): Second number converted from the code.
+    Parameters:
+    ----------
+        code : str
+            Country code with 2 letter.
 
-    Return:
-        A string number of fnum and snum.
+    Returns:
+    --------
+        result : str
+            A string number of fnum and snum.
+
+    Raises:
+    -------
+
     """
 
     let_num = []
+    path = file_path("resources", "letter_to_number")
 
-    # Open country code file
-    with open("../resources/letter_to_number.csv") as file:
-        reader = csv.reader(file)
-        header = next(reader)
+    if os.path.isfile(path):
 
-        # Write data to array
-        if header is not None:
-            for row in reader:
-                let_num.append(row)
+        # Open country code file
+        with open(path) as file:
+            reader = csv.reader(file)
+            header = next(reader)
 
-    # Convert country code to string of number
-    # First letter
-    for i in let_num:
-        if code[1] == let_num[i][0]:
-            fnum = let_num[i][1]
-            break
+            # Write data to array
+            if header is not None:
+                for row in reader:
+                    let_num.append(row)
 
-    # Second letter
-    for i in let_num:
-        if code[1] == let_num[i][0]:
-            snum = let_num[i][1]
-            break
+        # Convert country code to string of number
+        # First letter
+        for i in let_num:
+            if code[1] == let_num[i][0]:
+                fnum = let_num[i][1]
+                break
 
-    return str(fnum) + str(snum)
+        # Second letter
+        for i in let_num:
+            if code[1] == let_num[i][0]:
+                snum = let_num[i][1]
+                break
+
+        result = str(fnum) + str(snum)
+
+    return result
