be0d5dae586e1b2d9365c429453a4b3de04d335c
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-05-20 23:56:16 +0300

    update code

--- html_css/todolist_190520/asset/script.js
+++ html_css/todolist_190520/asset/script.js
@@ -8,12 +8,25 @@ window.onload = () => {
     }
   });
 
-  // Checked and unchecked items by click
   let list = document.querySelector("ul");
   list.addEventListener("click", (e) => {
     let targetItem = e.target;
-    if (targetItem.tagName === "LI") {
+
+    // Checked and unchecked items by click
+    if (targetItem.tagName === "SPAN") {
       targetItem.classList.toggle("done-task");
+    }
+
+    // Edit Item
+    if (targetItem.className === "fas fa-pen") {
+      editItem();
+    }
+
+    // Remove Item
+    if (targetItem.className === "fas fa-minus-circle") {
+      let line = targetItem.parentNode;
+      let thisLi = line.parentNode;
+      thisLi.removeChild(line);
     }
   });
 };
@@ -21,7 +34,13 @@ window.onload = () => {
 // Add Items function
 function addItem() {
   let item = document.getElementById("inputItem");
+  let span = document.createElement("span");
+  let edit = document.createElement("i");
+  let remove = document.createElement("i");
   let node = document.createElement("li");
+
+  edit.className = "fas fa-pen";
+  remove.className = "fas fa-minus-circle";
 
   // Alert if the input task is empty
   if (item === null) {
@@ -29,8 +48,12 @@ function addItem() {
       "The task name is empty! Please enter again!";
   }
 
-  let textnode = document.createTextNode(item.value);
-  node.appendChild(textnode);
+  span.innerHTML = item.value;
+
+  node.appendChild(span);
+  node.appendChild(edit);
+  node.appendChild(remove);
+
   document.getElementById("list").appendChild(node);
 
   // Clear input form after submit
