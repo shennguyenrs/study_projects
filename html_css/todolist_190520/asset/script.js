07fd8d1c414cfa1186219f4be24e35b923edf7d4
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-05-22 12:16:19 +0300

    update code

--- html_css/todolist_190520/asset/script.js
+++ html_css/todolist_190520/asset/script.js
@@ -9,32 +9,41 @@ window.onload = () => {
   });
 
   let list = document.querySelector("ul");
+  // Double click to mask as done
+  list.addEventListener("dblclick", (e) => {
+    let targetItem = e.target;
+
+    if (targetItem.tagName === "SPAN") {
+      let parent = targetItem.parentNode;
+      if (!parent.classList.contains("on-edit")) {
+        targetItem.classList.toggle("done");
+      }
+    }
+  });
+
+  // Click to edit or remove item
   list.addEventListener("click", (e) => {
     let targetItem = e.target;
 
-    // Checked and unchecked items by click
-    if (targetItem.tagName === "SPAN") {
-      targetItem.classList.toggle("done-task");
-    }
-
     // Edit Item
     if (targetItem.className === "fas fa-pen") {
-      editItem();
+      editItem(targetItem);
     }
 
     // Remove Item
     if (targetItem.className === "fas fa-minus-circle") {
       let line = targetItem.parentNode;
-      let thisLi = line.parentNode;
-      thisLi.removeChild(line);
+      let ul = line.parentNode;
+      ul.removeChild(line);
     }
   });
 };
 
 // Add Items function
 function addItem() {
-  let item = document.getElementById("inputItem");
+  let item = document.getElementById("inputItem").value;
   let span = document.createElement("span");
+  let inputform = document.createElement("input");
   let edit = document.createElement("i");
   let remove = document.createElement("i");
   let node = document.createElement("li");
@@ -43,19 +52,44 @@ function addItem() {
   remove.className = "fas fa-minus-circle";
 
   // Alert if the input task is empty
-  if (item === null) {
+  if (item.length === 0) {
     document.getElementById("message").innerHTML =
       "The task name is empty! Please enter again!";
+  } else {
+    span.innerText = item;
+    inputform.type = "text";
+
+    node.appendChild(span);
+    node.appendChild(inputform);
+    node.appendChild(edit);
+    node.appendChild(remove);
+
+    document.getElementById("list").appendChild(node);
+
+    // Clear input form after submit
+    item.value = "";
   }
+}
 
-  span.innerHTML = item.value;
+// Edit Item function
+function editItem(targetItem) {
+  let line = targetItem.parentNode;
+  let span = line.getElementsByTagName("SPAN");
+  let inputform = line.getElementsByTagName("INPUT");
 
-  node.appendChild(span);
-  node.appendChild(edit);
-  node.appendChild(remove);
+  inputform.value = span.innerText;
+  console.log(span.innerHTML);
 
-  document.getElementById("list").appendChild(node);
+  // Change style of editing form
+  line.classList.toggle("on-edit", true);
+  inputform.focus();
 
-  // Clear input form after submit
-  item.value = "";
+  // Listen for Enter key to finish
+  inputform.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      span.innerText = inputform.value;
+      line.classList.toggle("on-edit", false);
+    }
+  });
 }
