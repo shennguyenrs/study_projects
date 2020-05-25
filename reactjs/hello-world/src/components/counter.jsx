7b1bf6d1ce607b0ceaf23183413e9d5137ec0a2f
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-05-26 00:00:09 +0300

    new react learn project

---
+++ reactjs/hello-world/src/components/counter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+export default class counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  render() {
+    return (
+      <>
+        <span style={{ fontSize: 20 }} className={this.badgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increments
+        </button>
+        <ul>{this.renderTags()}</ul>
+      </>
+    );
+  }
+
+  formatCount() {
+    let { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  badgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There no tags!</p>;
+    return this.state.tags.map((tag) => <li key={tag}>{tag}</li>);
+  }
+
+  handleIncrement() {
+    console.log("Increment clicked");
+    return (this.state.count += 1);
+  }
+}
