c0040a881478de904154115990b98bbad63583fd
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-06-14 18:49:20 +0300

    update code

---
+++ reactjs/movies-app/src/components/common/form.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import Joi from "@hapi/joi";
+import Input from "./input";
+
+export default class form extends Component {
+  state = { account: {}, errors: {} };
+
+  validateProperty = ({ name, value }) => {
+    let object = { [name]: value };
+    let schema = { [name]: this.schema[name] };
+    let { error } = Joi.validate(object, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  validate = () => {
+    let { account } = this.state;
+    let { error } = Joi.validate(account, this.schema, { abortEarly: false });
+
+    if (!error) return null;
+    let errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return console.log(errors);
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    let errors = { ...this.state.errors };
+    let errorMessages = this.validateProperty(input);
+    if (errorMessages) errors[input.name] = errorMessages;
+    else delete errors[input.name];
+
+    let account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account, errors });
+  };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput({ name, label }) {
+    let { account, error } = this.state;
+    return (
+      <Input
+        name={name}
+        type={name}
+        label={label}
+        value={account[name]}
+        error={error[name]}
+        handleChange={this.handleChange}
+      />
+    );
+  }
+}
--- reactjs/movies-app/src/components/common/login.jsx
+++ reactjs/movies-app/src/components/common/login.jsx
@@ -1,62 +1,33 @@
-import React, { Component } from "react";
-import Joi from "joi";
-import Input from "./input";
+import React from "react";
+import Joi from "@hapi/joi";
+import Form from "./form";
 
-export default class login extends Component {
+export default class login extends Form {
   state = { account: { email: "", password: "" }, errors: {} };
 
   schema = {
-    username: Joi.require().label("Username"),
-    password: Joi.require().label("Password"),
+    email: Joi.string()
+      .email()
+      .min(3)
+      .max(30)
+      .required()
+      .label("Email Address"),
+    password: Joi.string().min(8).max(16).required().label("Password"),
   };
 
-  validateProperty = () => {};
-
-  validate = () => {
-    let errors = {};
-    let { account } = this.state;
-
-    if (account.email === "") errors.email = "Email is required";
-    if (account.password === "") errors.password = "Password is required";
-    return Object.keys(errors).length === 0 ? null : errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    let errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return console.log(errors);
-    return console.log("Submitted");
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    let account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account });
+  doSubmit = () => {
+    this.handleSubmit();
+    // Call Backend
+    console.log("Submitted");
   };
 
   render() {
-    const { account, errors } = this.state;
-
     return (
       <div className="container p-4">
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="email"
-            label="Email Address"
-            value={account.email}
-            error={errors.email}
-            handleChange={this.handleChange}
-          />
-          <Input
-            name="password"
-            label="Password"
-            value={account.password}
-            error={errors.password}
-            handleChange={this.handleChange}
-          />
-          <button className="btn btn-primary">Login</button>
+          {this.renderInput("email", "Email Address")}
+          {this.renderInput("password", "Password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
