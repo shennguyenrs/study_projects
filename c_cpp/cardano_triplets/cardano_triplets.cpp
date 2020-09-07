73fe3ba3876aa1704d3d5e3275c9bdc83c63d73f
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-09-07 22:50:52 +0300

    new code

--- c_cpp/cardano_triplets/cardano_triplets.cpp
+++ c_cpp/cardano_triplets/cardano_triplets.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 
 // Test integer number
-bool TestInteger(float c)
-{
-  float fracPart = c - int(c);
-  return fracPart > 0 ? false : true;
-}
+/*
+ *bool TestInteger(float c)
+ *{
+ *  float fracPart = c - int(c);
+ *  return fracPart > 0 ? false : true;
+ *}
+ */
 
 int main()
 {
   // Declare a, b and c followed the equation
-  unsigned int a=1, b=1, leftPart, sum=a+b;
-  float c;
+  unsigned int a=1, b=1, c=1, leftPart, denomirator;
+  //float c;
 
   // Declare variables
   unsigned int q, n, i=0, count=0;
@@ -31,23 +33,38 @@ int main()
 
   for(unsigned int x: vec)
   {
-    while(sum<=x)
+    while(a+b<x)
     {
-      while(sum-a<=x)
+      while(a+b<x)
       {
         leftPart = 8*pow(a,3) + 15*pow(a,2) + 6*a - 1; // Take from the equation
-        c = leftPart/(27.0*pow(b,2)); // Take from the equation
-        sum = a+b+c;
+        denomirator = 27*pow(b,2); //Take the equation
+
+        if(leftPart<denomirator)
+        {
+          break;
+        }
+
+        if((leftPart%denomirator)==0)
+        {
+          c = leftPart/(27*pow(b,2)); // Take from the equation
+          unsigned int sum = a+b+c;
+          count = sum<=x ? count+1 : count+0;
+        }
+        //c = leftPart/(27.0*pow(b,2)); // Take from the equation
+        //int sum = a+b+c;
 
         // If c is an integer and sum
-        count = (TestInteger(c) && sum<=x) ? count+1 : count+0;
+        //count = (TestInteger(c) && sum<=x) ? count+1 : count+0;
 
         // Increment b
         b++;
+        c=1;
       }
       
       // Increment a
       a++;
+      b=1;
     }
 
     cout << count << endl;
