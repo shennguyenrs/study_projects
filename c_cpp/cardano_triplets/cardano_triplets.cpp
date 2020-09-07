6037172cb9e21915aef98d264d0481ffd46e18e1
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-09-08 00:05:48 +0300

    update code

--- c_cpp/cardano_triplets/cardano_triplets.cpp
+++ c_cpp/cardano_triplets/cardano_triplets.cpp
@@ -3,20 +3,11 @@
 #include<cmath>
 using namespace std;
 
-// Test integer number
-/*
- *bool TestInteger(float c)
- *{
- *  float fracPart = c - int(c);
- *  return fracPart > 0 ? false : true;
- *}
- */
-
 int main()
 {
   // Declare a, b and c followed the equation
-  unsigned int a=1, b=1, c=1, leftPart, denomirator;
-  //float c;
+  unsigned int a=1, b=1, c=1;
+  double leftPart, denomirator;
 
   // Declare variables
   unsigned int q, n, i=0, count=0;
@@ -40,22 +31,19 @@ int main()
         leftPart = 8*pow(a,3) + 15*pow(a,2) + 6*a - 1; // Take from the equation
         denomirator = 27*pow(b,2); //Take the equation
 
+        // Skip if the fraction is below than 1
         if(leftPart<denomirator)
         {
           break;
         }
 
-        if((leftPart%denomirator)==0)
+        // Skip if c is not an integer
+        if(fmod(leftPart, denomirator)==0)
         {
           c = leftPart/(27*pow(b,2)); // Take from the equation
           unsigned int sum = a+b+c;
           count = sum<=x ? count+1 : count+0;
         }
-        //c = leftPart/(27.0*pow(b,2)); // Take from the equation
-        //int sum = a+b+c;
-
-        // If c is an integer and sum
-        //count = (TestInteger(c) && sum<=x) ? count+1 : count+0;
 
         // Increment b
         b++;
