677ee2ac160fef0c9fdacfcdb310adf72730050c
An Nguyen <shennguyen.redshinobi96@gmail.com>
2020-11-26 23:09:22 +0200

    update code

---
+++ c_cpp/11182020/midterm/hello.c
@@ -0,0 +1,14 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+int init_module(void)
+{
+  printk(KERN_INFO "Hello World\n");
+  return 0;
+}
+
+void cleanup_module(void)
+{
+  printk(KERN_INFO "Goodbye world\n");
+}
