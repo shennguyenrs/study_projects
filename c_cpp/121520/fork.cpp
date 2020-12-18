269625dad16527fbf84faa0fc9d2441b04250219
shennguyenrs <shennguyen.redshinobi96@gmail.com>
2020-12-18 11:05:35 +0200

    update code

--- c_cpp/121520/fork.cpp
+++ c_cpp/121520/fork.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <mutex>
+#include <unistd.h>
+#include <sys/wait.h>
 
-using namespace std;
+std::mutex locker; 
 
-int main()
-{
-  return 0;
+void job(std::ofstream& file) 
+{ 
+  locker.lock();
+
+  file << "Enter from " << getpid() << std::endl;
+
+  for(size_t i{0}; i<=10; i++)
+  {
+    file << i << std::endl;
+  }
+
+  file << "Exit from " << getpid() << std::endl;
+
+  locker.unlock();
 }
+
+int main() 
+{  
+  std::ofstream file;
+
+  file.open("test.txt");
+
+  pid_t pid = fork();
+
+  if(pid==0)
+  {
+    file << "Child Process" << std::endl;
+    job(file);
+    exit(0);
+  }
+  else
+  {
+    waitpid(pid, NULL, 0);
+    file << "Parent Process" << std::endl;
+    job(file);
+  }
+
+  file.close();
+
+	return 0; 
+} 
--- c_cpp/121520/thread.cpp
+++ c_cpp/121520/thread.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <mutex>
+#include <unistd.h>
+#include <thread>
 
-using namespace std;
+std::mutex locker; 
 
-int main()
-{
-  return 0;
+void job(std::ofstream& file) 
+{ 
+  locker.lock(); 
+
+  std::thread::id this_id = std::this_thread::get_id();
+
+  file << "Enter thread ID: " << this_id << std::endl;
+
+  for(size_t i{0}; i<=10; i++)
+  {
+    file << i << std::endl;
+  }
+
+  file << "Exit from thread " << this_id << std::endl;
+
+  locker.unlock();
 }
+
+int main() 
+{  
+  std::ofstream file;
+
+  file.open("test.txt");
+
+  std::thread th1( [&]
+  {
+    job(file);
+  });
+
+  std::thread th2( [&]
+  {
+    job(file);
+  });
+
+  th1.join();
+  th2.join();
+
+  file.close();
+
+	return 0; 
+} 
