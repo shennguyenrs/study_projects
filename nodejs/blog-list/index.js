e3a40eb7de11da49c33f78c814058cef4c14f0f3
shennguyenrs <shennguyen.redshinobi96@gmail.com>
2021-02-01 23:59:12 +0200

    update code

--- nodejs/blog-list/index.js
+++ nodejs/blog-list/index.js
@@ -1,15 +1,43 @@
-const palindrome = (string) => {
-  return string.split('').reverse().join('');
-};
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const average = (array) => {
-  const reducer = (sum, item) => {
-    return sum + item;
-  };
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  return array.reduce(reducer, 0) / array.length;
+const Blog = mongoose.model('Blog', blogSchema)
 
-  //return array.length === 0 ? 0 : array.reduce(reducer, 0);
-};
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-module.exports = { palindrome, average };
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
