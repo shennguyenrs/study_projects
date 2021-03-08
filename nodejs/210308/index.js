461e1d13382fafad1eaeb5dba82dd9a13f79207c
An Nguyen <shennguyen.redshinobi96@gmail.com>
2021-03-08 14:01:44 +0200

    update code

--- nodejs/210308/index.js
+++ nodejs/210308/index.js
@@ -2,45 +2,62 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(express.json());
 
 let customers = [
-  {id: '1588323375416', firstName: 'John', lastName: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
-  {id: '1588323375417', firstName: 'Mary', lastName: 'Smith', email: 'mary@smith.com', phone: '6654113'},
-  {id: '1588323375418', firstName: 'Peter', lastName: 'North', email: 'peter@north.com', phone: '901176'},
-]
+  {
+    id: '1588323375416',
+    firstName: 'John',
+    lastName: 'Johnson',
+    email: 'john@johnson.com',
+    phone: '8233243',
+  },
+  {
+    id: '1588323375417',
+    firstName: 'Mary',
+    lastName: 'Smith',
+    email: 'mary@smith.com',
+    phone: '6654113',
+  },
+  {
+    id: '1588323375418',
+    firstName: 'Peter',
+    lastName: 'North',
+    email: 'peter@north.com',
+    phone: '901176',
+  },
+];
 
-app.get("/api/customers", (req, res) => {
-    res.json(customers);
-})
+app.get('/api/customers', (_req, res) => {
+  res.json(customers);
+});
 
-app.get("/api/customers/:id", (req, res) => {
-    const id = req.params.id;
-    customer = customers.filter(cus => cus.id === id);
-    res.json(customer);
-})
-
-app.post("/api/customers", (req, res) => {
-    const newCus = {'id': Date.now(), ...req.body};
-    customers = [...customers, newCus];
-    res.json(newCus);
-})
-
-app.delete("/api/customers/:id", (req, res) => {
-    const id = req.params.id;
-    customers = customers.filter(cus => cus.id !== id);
-    res.status(204).end()
-})
-
-app.put("/api/customers/:id", (req, res) => { 
+app.get('/api/customers/:id', (req, res) => {
   const id = req.params.id;
-  const updatedCus = {'id': id, ...req.body};
-  const index = customers.findIndex(cus => cus.id === id);
-  customers.splice(index, 1, updatedCus); 
+  const customer = customers.filter((cus) => cus.id === id);
+  res.json(customer);
+});
+
+app.post('/api/customers', (req, res) => {
+  const newCus = { id: Date.now(), ...req.body };
+  customers = [...customers, newCus];
+  res.json(newCus);
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  const id = req.params.id;
+  customers = customers.filter((cus) => cus.id !== id);
+  res.status(204).end();
+});
+
+app.put('/api/customers/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedCus = { id: id, ...req.body };
+  const index = customers.findIndex((cus) => cus.id === id);
+  customers.splice(index, 1, updatedCus);
   res.json(updatedCus);
-})
+});
 
 app.listen(port, () => {
-    console.log("App is listening on port "+port);
-})
+  console.log('App is listening on port ' + port);
+});
